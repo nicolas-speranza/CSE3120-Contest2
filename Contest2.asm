c5b835168435161a6302268b26cb6da09ab58ec3
nicolas-speranza <nrinconspera2022@my.fit.edu>
2026-04-16 19:46:21 -0400

    Initialize Byte Hopper title screen

--- Contest2.asm
+++ Contest2.asm
@@ -1,10 +1,26 @@
 INCLUDE Irvine32.inc
 
 .data
-
+titleMsg BYTE "BYTE HOPPER",0
+startMsg BYTE "Press any key to start...",0
 
 .code
 main PROC
+    call Clrscr
+    mov dh,10
+    mov dl,30
+    call Gotoxy
+    mov edx,OFFSET titleMsg
+    call WriteString
+
+    mov dh,12
+    mov dl,24
+    call Gotoxy
+    mov edx,OFFSET startMsg
+    call WriteString
+
+    call ReadChar
+    exit
 
 main ENDP
 END main
